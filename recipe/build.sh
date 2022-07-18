360f51eae19dd73987f5650c5dd6e727e950dff4
Serhii Kupriienko <skupriienko@anaconda.com>
2022-07-18 08:59:01 +0000

    Do not copy libXi

--- recipe/build.sh
+++ recipe/build.sh
@@ -33,8 +33,3 @@ bin/gtk3-demo*
 bin/gtk3-icon-browser
 bin/gtk3-widget-factory
 ")
-
-# 2022/7/18 SK: Copy over DSOs to resolve missing libXi.so.6 error on linux-aarch64
-#if [[ $target_platform == linux-aarch64 ]]; then
-#  cp -n ${BUILD_PREFIX}/aarch64-conda-linux-gnu/sysroot/usr/lib64/libXi.so* ${PREFIX}/lib/
-#fi
