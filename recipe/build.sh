623fac35d935756b62cf012acee584c396ef7384
Viacheslav Ch. <vchychyrko@anaconda.com>
2026-03-04 20:28:16 +0200

    cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,21 +8,6 @@ export PKG_CONFIG="$BUILD_PREFIX/bin/pkg-config"
 export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig:${PKG_CONFIG_PATH:-}"
 export CMAKE_PREFIX_PATH="$PREFIX:${CMAKE_PREFIX_PATH:-}"
 
-# make sure pkg-config file name matches what meson expects
-if [[ -f "$PREFIX/lib/pkgconfig/libepoxy.pc" && ! -f "$PREFIX/lib/pkgconfig/epoxy.pc" ]]; then
-  ln -s "$PREFIX/lib/pkgconfig/libepoxy.pc" "$PREFIX/lib/pkgconfig/epoxy.pc"
-fi
-if [[ -f "$PREFIX/lib64/pkgconfig/libepoxy.pc" && ! -f "$PREFIX/lib64/pkgconfig/epoxy.pc" ]]; then
-  ln -s "$PREFIX/lib64/pkgconfig/libepoxy.pc" "$PREFIX/lib64/pkgconfig/epoxy.pc"
-fi
-
-
-echo "=== epoxy pkg-config check ==="
-ls -la "$PREFIX/lib/pkgconfig" "$PREFIX/lib64/pkgconfig" | grep -i epoxy || true
-$PKG_CONFIG --print-errors --list-all | grep -i epoxy || true
-$PKG_CONFIG --print-errors --modversion epoxy || true
-echo "=== epoxy pkg-config check END ==="
-
 meson_config_args=(
     -D gtk_doc=false
     -D demos=false
